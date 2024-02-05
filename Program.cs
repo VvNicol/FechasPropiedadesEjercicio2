5aadec8496eb6aaa234e371120896dc14f3bd3f1
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-02-05 12:37:10 +0100

    Fechas parametros

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DateTime fechaHoyPropiedad = DateTime.Today;
+
+            int dia = fechaHoyPropiedad.Day;
+            int month = fechaHoyPropiedad.Month;
+            int year = fechaHoyPropiedad.Year;
+            int hour = fechaHoyPropiedad.Hour;
+            int min = fechaHoyPropiedad.Minute;
+            int seg = fechaHoyPropiedad.Second;
+            _ = fechaHoyPropiedad.DayOfWeek;
+            int yeary = fechaHoyPropiedad.DayOfYear;
         }
     }
 }
